48a858f09870007b74baea9e0370cb8919d18481
mahdi <0059sharifloo@gmail.com>
2023-05-25 22:32:14 +0330

    update new design

--- Dockerfile
+++ Dockerfile
@@ -9,10 +9,14 @@ COPY requirements.txt .
 RUN pip install --upgrade pip
 RUN pip install --no-cache-dir -r requirements.txt
 
+
 COPY . .
 
 # running migrations
-RUN python manage.py migrate
+# RUN python manage.py migrate
 
 # gunicorn
 CMD ["gunicorn", "--config", "gunicorn-cfg.py", "core.wsgi"]
+
+# you have to run python manage.py migrate after gunicord run.
+# it's means that you have to login to docker container and make migrations.
--- manage.py
+++ manage.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
 import os
 import sys
 
-
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,6 +18,5 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
